b76d85943b20e27170f21fba3f117b7a4c55e016
clueless-bachu <vasista1997@gmail.com>
2018-10-20 12:08:33 +0400

    Just added a few comments about the PID controller

--- pid_controller.py
+++ pid_controller.py
@@ -7,15 +7,19 @@ class PID_Controller():
 
 
         self.gui = parent_gui
-
+        ## Initializing all the constants
         self.kp = self.gui.slide_p.get()
         self.kd = self.gui.slide_d.get()
         self.ki = self.gui.slide_i.get()
 
         self._dt = dt
 
+        # e_integrated is the numerical integration of all errors which is multiplied with Ki
         self.e_integrated = 0
+        
+        # e_prev is the previous iteration error. This is used to perform numerical Differentiation and is multiplied with Kd
         self.e_prev = None
+        
         self.control_output = [0,0,0]
 
     def run(self,state,reference):
